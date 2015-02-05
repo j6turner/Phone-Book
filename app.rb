1c482b8dfdf99d267fa05fd896cc7d1d88b9bb62
J. TURNER <turner.j6@gmail.com>
2015-02-05 15:36:56 -0800

    Reconfigure app.rb.

--- app.rb
+++ app.rb
@@ -20,24 +20,19 @@ post('/clear') do
   redirect("/")
 end
 
-post('/phonebook/:id') do
-  type = params.fetch("type")
-  number = params.fetch("number")
-  @phone = Phone.new({ :type => type, :number => number })
-  @contact_new = Contact.find(params.fetch("id").to_i())
-  @contact.add_number(@phone)
-  redirect("/contacts/#{contact_new}")
-end
-
 get('/contacts/:id') do
   @contact = Contact.find(params.fetch("id").to_i())
   @contacts = Contact.all()
-  Phone.all(@contact).phone_numbers()
   erb(:contact)
 end
 
-get('/phonebook/:name') do
-  found_contact = Contact.find(params.fetch("name"))
-  @name = found_contact.name()
-  erb(:contact)
+post('/number') do
+  type = params.fetch("type")
+  number = params.fetch("number")
+  @phone = Phone.new("type", "number")
+  @phone.save()
+  @contact = Contact.find(params.fetch("id").to_i())
+  @contacts = Contact.all()
+  @contact.add_number(phone)
+  redirect back
 end
