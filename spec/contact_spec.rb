30672f9d4c01ecbcd3dd84ddb4e502114f8c3538
J. TURNER <turner.j6@gmail.com>
2015-02-14 19:39:35 -0800

    Adjust indentation in spec/contact_spec.rb.

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -42,23 +42,23 @@ describe("Contact") do
     end
   end
 
-    describe('#id') do
-      it('returns contact id') do
-        test_contact = Contact.new("Axyll Adams")
-        test_contact.save()
-        expect(test_contact.id()).to(eq(1))
-      end
+  describe('#id') do
+    it('returns contact id') do
+      test_contact = Contact.new("Axyll Adams")
+      test_contact.save()
+      expect(test_contact.id()).to(eq(1))
     end
-
-    describe('#add_number') do
-      it('adds phone number to contact') do
-        test_number = Phone.new("cell", "1112223333")
-        test_number.save()
-        test_contact = Contact.new("Axyll Adams")
-        test_contact.save()
-        test_contact.add_number(test_number)
-        expect(test_contact.phone_numbers()).to(eq([test_number]))
-      end
-    end
-
   end
+
+  describe('#add_number') do
+    it('adds phone number to contact') do
+      test_number = Phone.new("cell", "1112223333")
+      test_number.save()
+      test_contact = Contact.new("Axyll Adams")
+      test_contact.save()
+      test_contact.add_number(test_number)
+      expect(test_contact.phone_numbers()).to(eq([test_number]))
+    end
+  end
+
+end
